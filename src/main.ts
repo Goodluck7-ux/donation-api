ce7a3dd53d919e68d929f292076ffddaa78f296a
Goodluck7-ux <goodluckmichael503@gmail.com>
2026-08-26 10:53:29 +0100

    fix:updated package.json

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,9 @@ setDefaultResultOrder('ipv4first');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3001',
     credentials: true,
-  });
+});
 
   app.use(
     express.json({
